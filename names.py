d40f15a9ce77faf291c59f56d975308475b8b54c
matecelaa <mcelaa@mail.austral.edu.ar>
2026-03-27 10:36:20 -0300

    Add files via upload

--- names.py
+++ names.py
@@ -4,7 +4,7 @@ def names():
     """
     
     nombre = input ("Ingrese su nombre: ")
-    apellido = input("Ingrese su apellido")
+    apellido = input("Ingrese su apellido: ")
     nombre_completo = nombre + " " + apellido
     print(nombre_completo.lower())
     print(nombre_completo.title())
